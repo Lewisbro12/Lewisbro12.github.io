b3924569c5d07dfedc21b40cab31e65cbf97e331
Lewisbro12 <44899657+Lewisbro12@users.noreply.github.com>
2020-08-14 06:02:21 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,12 @@
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var d = new Date();
-var hr = d.getHours();
-var min = d.getMinutes();
-if (min < 10) {
-    min = "0" + min;
-}
-var ampm = "am";
-if( hr > 12 ) {
-    hr -= 12;
-    ampm = "pm";
-}
+
 var date = d.getDate();
 function thstndrd(n) {
     return [`${n}st`,`${n}nd`,`${n}rd`][(((n < 0 ? -n : n) + 90 ) % 100 - 10) % 10 - 1] || `${n}th`;
 }
 var month = months[d.getMonth()];
 var year = d.getFullYear();
-var x = document.getElementById("time");
-var z = document.getElementById("date");
-x.innerHTML = hr + ":" + min + ampm;
-z.innerHTML = thstndrd(date) + " " + day + " " + month + " " + year;
+var x = document.getElementById("date");
+x.innerHTML = thstndrd(date) + " " + day + " " + month + " " + year;
